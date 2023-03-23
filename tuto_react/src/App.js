0bcaf26356f65384b8cd502fe3f8032e771d9d05
jeckie42 <117743205+jeckie42@users.noreply.github.com>
2023-03-23 14:56:08 +0100

    first message

---
+++ tuto_react/src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const list = [
+    "Facebook",
+    "Social Media",
+    "Java script",
+    "Watches",
+    "Password",
+    "Money script"
+  ];
+  const [filterList, setFilterList] = useState(list);
+  const handleSearch = (event) => {
+    if (event.target.value === "") {
+      setFilterList(list);
+      return;
+    }
+
+    const filteredValues = list.filter(
+      (item) => item.toLowerCase().indexOf(event.target.value.toLowerCase()) !== -1
+    );
+    setFilterList(filteredValues)
+  };
+
+  return (
+    <div className="App">
+      <div>
+              Search: <input name="query" type="text" onChange={handleSearch} />
+      </div>
+      {filterList &&
+        filterList.map((item, index) => (
+          <div key={index}>{item}</div> //Display each item
+        ))}
+    </div>
+  );
+}
+export default App;
